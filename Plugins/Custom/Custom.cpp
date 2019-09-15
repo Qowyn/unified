bb7a9b2351a3f5243bc5e13ea6b644e6bf78d719
Qowyn <qowync@gmail.com>
2019-09-15 02:27:55 +0200

    Possess, Unpossess, Ammo fix

--- Plugins/Custom/Custom.cpp
+++ Plugins/Custom/Custom.cpp
@@ -1,11 +1,15 @@
 #include "Custom.hpp"
+#include "API/CAppManager.hpp"
+#include "API/CServerExoApp.hpp"
 #include "API/CNWSCombatRound.hpp"
 #include "API/CNWSCreature.hpp"
 #include "API/CNWSCreatureStats.hpp"
+#include "API/CNWSPlayer.hpp"
 #include "API/Functions.hpp"
 #include "API/Globals.hpp"
 #include "API/Constants.hpp"
 #include "API/Types.hpp"
+#include "Services/Events/Events.hpp"
 #include "Services/Hooks/Hooks.hpp"
 #include "Services/Messaging/Messaging.hpp"
 #include "ViewPtr.hpp"
@@ -43,12 +47,69 @@ static NWNXLib::Hooking::FunctionHook* m_InitializeNumberOfAttacksHook;
 Custom::Custom(const Plugin::CreateParams& params)
     : Plugin(params)
 {
+#define REGISTER(func) \
+    GetServices()->m_events->RegisterEvent(#func, std::bind(&Custom::func, this, std::placeholders::_1))
+
+    REGISTER(PossessCreature);
+    REGISTER(UnpossessCreature);
+#undef REGISTER
     GetServices()->m_hooks->RequestExclusiveHook<API::Functions::CNWSCombatRound__InitializeNumberOfAttacks>(&InitializeNumberOfAttacksHook);
     m_InitializeNumberOfAttacksHook = GetServices()->m_hooks->FindHookByAddress(Functions::CNWSCombatRound__InitializeNumberOfAttacks);
 }
 
 Custom::~Custom()
 {
+}
+
+ArgumentStack Custom::PossessCreature(ArgumentStack&& args)
+{
+    ArgumentStack stack;
+    if (auto *pPlayer = player(args))
+    {
+        const auto creatureId = Services::Events::ExtractArgument<Types::ObjectID>(args);
+        auto nMode = Services::Events::ExtractArgument<int32_t>(args);
+
+        auto *pCreature = Globals::AppManager()->m_pServerExoApp->GetCreatureByGameObjectID(pPlayer->m_oidPCObject);
+        if (pCreature)
+        {
+            pCreature->PossessCreatureDM(creatureId, nMode);
+        }
+    }
+
+    return stack;
+}
+
+ArgumentStack Custom::UnpossessCreature(ArgumentStack&& args)
+{
+    ArgumentStack stack;
+    if (auto *pPlayer = player(args))
+    {
+        auto *pCreature = Globals::AppManager()->m_pServerExoApp->GetCreatureByGameObjectID(pPlayer->m_oidPCObject);
+        if (pCreature)
+        {
+            pCreature->UnpossessCreatureDM();
+        }
+    }
+
+    return stack;
+}
+
+CNWSPlayer *Custom::player(ArgumentStack& args)
+{
+    const auto playerId = Services::Events::ExtractArgument<Types::ObjectID>(args);
+
+    if (playerId == Constants::OBJECT_INVALID)
+    {
+        LOG_NOTICE("NWNX_Custom function called on OBJECT_INVALID");
+        return nullptr;
+    }
+
+    auto *pPlayer = Globals::AppManager()->m_pServerExoApp->GetClientObjectByObjectId(playerId);
+    if (!pPlayer)
+    {
+        LOG_NOTICE("NWNX_Custom function called on non-player object %x", playerId);
+    }
+    return pPlayer;
 }
 
 void Custom::InitializeNumberOfAttacksHook(API::CNWSCombatRound* pRound)
--- Plugins/Custom/Custom.hpp
+++ Plugins/Custom/Custom.hpp
@@ -1,9 +1,13 @@
 #pragma once
 
 #include "Plugin.hpp"
+#include "Services/Events/Events.hpp"
 #include "API/CNWSCombatRound.hpp"
 #include "API/CNWSCreatureStats.hpp"
+#include "API/CNWSPlayer.hpp"
 #include "API/Types.hpp"
+
+using ArgumentStack = NWNXLib::Services::Events::ArgumentStack;
 
 namespace Custom {
 
@@ -14,6 +18,9 @@ public:
     virtual ~Custom();
 
 private:
+    ArgumentStack PossessCreature(ArgumentStack&& args);
+    ArgumentStack UnpossessCreature(ArgumentStack&& args);
+    NWNXLib::API::CNWSPlayer *player(ArgumentStack& args);
     static void InitializeNumberOfAttacksHook(NWNXLib::API::CNWSCombatRound*);
     static int GetLevelByClass(NWNXLib::API::CNWSCreatureStats *pStats, uint32_t nClassType);
 };
